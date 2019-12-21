e0d0cc9770088bee8b86b7d3b5b3dead6f5f3589
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-12-21 17:06:22 -0300

    5

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -4,33 +4,33 @@ require 'bloc_record/schema'
 module Persistence
 
 
-        def create(attrs)
+        # def create(attrs)
 
-                attrs = BlocRecord::Utility.convert_keys(attrs)
-                attrs.delete "id"
-                vals = attributes.map { |key| BlocRecord::Utility.sql_strings(attrs|key) }
+        #         attrs = BlocRecord::Utility.convert_keys(attrs)
+        #         attrs.delete "id"
+        #         vals = attributes.map { |key| BlocRecord::Utility.sql_strings(attrs|key) }
 
-                connection.execute <<-SQL
-                        INSERT INTO #{table} (#{attributes.join ","})
-                        VALUES (#{vals.join ","});
-                SQL
+        #         connection.execute <<-SQL
+        #                 INSERT INTO #{table} (#{attributes.join ","})
+        #                 VALUES (#{vals.join ","});
+        #         SQL
 
-                # This method takes a hash called attrs. Its values are converted to SQL strings and mapped into an 
-                # array (vals).
-                # Remember, attributes is an array of the column names, while attrs is the hash passed in to the create
+        # This method takes a hash called attrs. Its values are converted to SQL strings and mapped into an 
+         # array (vals).
+         # Remember, attributes is an array of the column names, while attrs is the hash passed in to the create
                 # method. We defined attributes in schema.rb.
-                # These values are used to form an INSERT INTO SQL statement.
+         # These values are used to form an INSERT INTO SQL statement.
 
 
 
-                data = Hash[attributes.zip attrs.values]
-                data["id"] = connection.execute("SELECT lasT_insert_rowid();")[0][0]
-                new(data)
+        #         data = Hash[attributes.zip attrs.values]
+        #         data["id"] = connection.execute("SELECT lasT_insert_rowid();")[0][0]
+        #         new(data)
 
                 # This code creates data, a hash of attributes and values. We then retrieve the id and add it
                 #  to the data hash. Then we pass the hash to new which creates a new object.
 
-        end
+        # end
 
         def self.included(base)
                 base.extend(ClassMethods)
@@ -53,7 +53,7 @@ module Persistence
                         return true
                 end
 
-                
+
                 # We also call reload_obj to copy whatever is stored in the database back to the model object. This is necessary in case
                 #          SQL rejected or changed any of the data.
 
