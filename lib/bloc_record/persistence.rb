468b1df5cd5da9f87f38ad1c79752c4f66a375e2
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-12-21 17:19:45 -0300

    6

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -4,33 +4,7 @@ require 'bloc_record/schema'
 module Persistence
 
 
-        # def create(attrs)
-
-        #         attrs = BlocRecord::Utility.convert_keys(attrs)
-        #         attrs.delete "id"
-        #         vals = attributes.map { |key| BlocRecord::Utility.sql_strings(attrs|key) }
-
-        #         connection.execute <<-SQL
-        #                 INSERT INTO #{table} (#{attributes.join ","})
-        #                 VALUES (#{vals.join ","});
-        #         SQL
-
-        # This method takes a hash called attrs. Its values are converted to SQL strings and mapped into an 
-         # array (vals).
-         # Remember, attributes is an array of the column names, while attrs is the hash passed in to the create
-                # method. We defined attributes in schema.rb.
-         # These values are used to form an INSERT INTO SQL statement.
-
-
-
-        #         data = Hash[attributes.zip attrs.values]
-        #         data["id"] = connection.execute("SELECT lasT_insert_rowid();")[0][0]
-        #         new(data)
-
-                # This code creates data, a hash of attributes and values. We then retrieve the id and add it
-                #  to the data hash. Then we pass the hash to new which creates a new object.
-
-        # end
+     
 
         def self.included(base)
                 base.extend(ClassMethods)
@@ -51,6 +25,7 @@ module Persistence
                         self.id = self.class.create(BlocRecord::Utility.instance_variables_to_hash(self)).id   
                         BlocRecord::Utility.reload_obj(self)
                         return true
+                     end
                 end
 
 
@@ -69,8 +44,9 @@ module Persistence
                 SQL
 
                 true
-
-        end
+              end
+       
+        
 
 
 
