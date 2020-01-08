d519747de955c9ae4bb699c2c0afea54100ee595
Eduardo Lowensohn <gunchosoft@gmail.com>
2020-01-07 21:57:29 -0300

    8

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -7,26 +7,38 @@ module Selection
     if ids.length == 1
       find_one(ids.first)
     else 
+      ids.each do |id|
+        if id.is_a?(Integer) && id > 0
+          next
+        else
+          puts "This is not a valid ID"
+          return -1
+        end
+      end
+    
       rows = connection.execute <<-SQL
-       SELECT #{columns.join ","} FROM #{table}
-       WHERE id IN (#{ids.join(",")});
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
       SQL 
       rows_to_array(rows)
     end
   end
+
+
   
-  # def find_one(id)
-  #   row = connection.get_first_row <<-SQL
-  #   SELECT #{columns.join ","} FROM #{table}
-  #   WHERE id = #{id};
-  #  SQL
-  #   init_object_from_row(row)
-  # end
-
-
-
-      
-
+  #  def find_one(id)
+  #   if id.is_a?(Integer) && id > 0
+  #    row = connection.get_first_row <<-SQL
+  #     SELECT #{columns.join ","} FROM #{table}
+  #     WHERE id = #{id};
+  #    SQL
+  #    init_object_from_row(row)
+  #   else
+  #     puts "This is not a valid ID"
+  #     return -1
+  #   end
+  #  end
+   
 
   def find_by(attribute, value)
     rows = connection.execute <<-SQL
@@ -84,10 +96,46 @@ module Selection
     SQL
     rows_to_array(rows)
   end
-
-  
-
 end
+
+def find_each(options={})
+  start = options[:start]
+  batch_size = options[:batch_size]
+  if start != nil && batch_size != nil
+    rows = connection.execute <<-SQL
+     SELECT #{columns.join ","} FROM #{table}
+     LIMIT #{batch_size} OFFSET #{start};
+    SQL
+  elseif start == nil && batch_size != nil
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size};
+    SQL
+  elseif start != nil && batch_size == nil
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      OFFSET #{start};
+    SQL
+  else
+    rows = connection.execute <<-SQL
+      SELCT #{columns.join ","} FROM #{table};
+    SQL
+  end
+
+  row_array = rows_to_array(rows)
+  yield(row_array)
+end
+
+def find_in_batches(start, batch_size)
+  rows = connection.execute <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    LIMIT #{batch_size} OFFSET #{start};
+  SQL
+  row_array = rows_to_array(rows)
+  yeld(row_array)
+end
+
+
 
 private
 
@@ -102,6 +150,24 @@ private
     rows.map { |row| new(Hash[columns.zip(row)])}
   end
 
+  def method_missing(method_name, *args)
+    if method_name.match(/find_by/)
+      attribute = method_name.to_s.split('find_by_')[1]
+      if columns.include?(attribute)
+        find_by(attribute, *args)
+      else
+        puts "#{attribute} is not at the DB"
+      end
+    else
+      puts "#{methid_name} is not available"
+      end
+  end
 
 end
+
+
+  
+
+
+
 
