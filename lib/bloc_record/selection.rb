8d2787daf0b3b2a080ee0e957ff350d06ec13fbc
Eduardo Lowensohn <gunchosoft@gmail.com>
2020-01-10 14:27:11 -0300

    w_o_j1

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -152,6 +152,69 @@ def find_one(id)
   end
  end
 
+def where(*args)
+  if args.count > 1
+    expression = args.shift
+    params = args
+  else
+      case args.first
+      when String
+        expression = args.first
+
+            #Entry.where("phone_number = '999-999-9999'")
+
+            
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }.join(" and ")
+      
+        #Entry.where(name: 'BlocHead')
+
+      end
+
+  end
+
+  sql = <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    WHERE #{expression};
+  SQL
+
+  rows = connection.execute(sql, params)
+  rows_to_array(rows)
+
+end
+
+# In this example, we're handling array input by checking whether args.count > 1.
+# This SQL statement is very familiar to us. The only change is, predictably, the WHERE clause, which scopes the
+#  response to args.shift. shift "removes the first element of self and returns it (shifting all other elements down by one)."
+# The remaining arguments are passed in to connection.execute, which handles the question mark replacement for us.
+
+
+# def order(order)
+#   order = order.to_s
+
+#  Entry.order("name", "phone_number") Entry.order(:name, :phone_number) To accomplish this, we'll use the splat
+ #operator, and join the elements if the resulting Array has multiple elements:
+
+def order(*args)
+  if args > 1
+    order = args.join(",")
+  else
+    order = args.first.to_s
+  end
+
+  rows = connection.execute <<-SQL
+    SELECT * FROM #{table}
+    ORDER BY #{order};
+  SQL
+  rows_to_array(rows)
+end
+
+
+
+  #If the order local variable is a Symbol, to_s will convert it to a string. If it's already a String,
+  #to_s will have no effect and the string will be interpolated directly into the SQL query.
+
 
 private
 
@@ -180,10 +243,6 @@ private
   end
 
 end
-
-
-  
-
 
 
 
